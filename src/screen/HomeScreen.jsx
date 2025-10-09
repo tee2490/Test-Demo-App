462d79f56e80f675b4b19ff6a2e167f1f6db1db2
Teeradet Tavarpinun <tee2490@hotmail.com>
2025-10-09 11:08:53 +0700

    Expo LinearGradient Background

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,15 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Platform, StyleSheet, View } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { Dimensions } from 'react-native';
 
-export default function HomeScreen() {
+const windowH = Dimensions.get('window').height; // พื้นที่แอปหลังหัก status bar/nav bar (ส่วนมาก)
+const screenH  = Dimensions.get('screen').height; // ความสูงเต็มหน้าจอ (รวมระบบ)
+
+const HomeScreen = () => {
   return (
-    <SafeAreaView edges={['top']} style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["rgba(0,0,0,0.5)", "transparent"]}
+        style={styles.background}
+      />
     </View>
-</SafeAreaView>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({})
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+   
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: windowH/2,
+  },
+  button: {
+    padding: 15,
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  text: {
+    backgroundColor: "transparent",
+    fontSize: 15,
+    color: "#fff",
+  },
+});
