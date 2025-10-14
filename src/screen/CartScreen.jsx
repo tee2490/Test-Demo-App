422adb34955ebc66e5e6ddd64f1b2481b611a31d
Teeradet Tavarpinun <tee2490@hotmail.com>
2025-10-14 09:42:32 +0700

    Cart Header Component – CartScreen Design

--- src/screen/CartScreen.jsx
+++ src/screen/CartScreen.jsx
@@ -1,7 +1,15 @@
-import { Platform, StyleSheet, Text, View } from "react-native";
+import {
+  TouchableOpacity,
+  FlatList,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import Header from "../components/Header";
+import CartProduct from "../components/CartProduct";
 
 const CartScreen = () => {
   return (
@@ -12,6 +20,37 @@ const CartScreen = () => {
         style={styles.background}
       />
       <Header isCart={true} />
+
+      <FlatList
+        data={[1, 2, 3, 4, 5]}
+        renderItem={({ item }) => <CartProduct />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ marginTop: 40, paddingBottom: 200 }}
+        ListFooterComponent={
+          <>
+            <View style={styles.bottomContentContainer}>
+              <View style={styles.flexRowContainer}>
+                <Text style={styles.titleText}>Total:</Text>
+                <Text style={styles.priceText}>$1000.00</Text>
+              </View>
+              <View style={styles.flexRowContainer}>
+                <Text style={styles.titleText}>Shpping:</Text>
+                <Text style={styles.priceText}>$50.0</Text>
+              </View>
+              <View style={styles.divider} />
+              <View style={styles.flexRowContainer}>
+                <Text style={styles.titleText}>Grand Total:</Text>
+                <Text style={[styles.priceText, styles.grandPriceText]}>
+                  $1050.00
+                </Text>
+              </View>
+            </View>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Checkout</Text>
+            </TouchableOpacity>
+          </>
+        }
+      />
     </View>
   );
 };
@@ -22,7 +61,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    //marginTop: Platform.OS == "android" ? 25 : 0,
+   // marginTop: Platform.OS == "android" ? 25 : 0,
     padding: 10,
   },
   background: {
@@ -31,5 +70,48 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     height: "100%",
+  },
+  header: {},
+  flexRowContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 5,
+  },
+  bottomContentContainer: {
+    marginHorizontal: 10,
+    marginTop: 30,
+  },
+  titleText: {
+    fontSize: 18,
+    color: "#757575",
+    fontWeight: "500",
+  },
+  priceText: {
+    fontSize: 18,
+    color: "#757575",
+    fontWeight: "600",
+  },
+  divider: {
+    borderWidth: 1,
+    borderColor: "#C0C0C0",
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  grandPriceText: {
+    color: "#3C3C3C",
+    fontWeight: "700",
+  },
+  button: {
+    backgroundColor: "#E96E6E",
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+    marginTop: 30,
+  },
+  buttonText: {
+    fontSize: 24,
+    color: "#FFFFFF",
+    fontWeight: "700",
   },
 });
